be8a4a81737bb5038b0eb423cd7b447a37205b6d
eduardo-diniz <eduardogomesdiniz42@gmail.com>
2021-11-23 19:33:34 -0300

    Criando Class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -9,11 +9,47 @@ public class PrimeiroPrograma {
 
         Gato gato = new Gato();
         System.out.println(gato);
-
+        Livro livro1 = new Livro("A historia do long", 300);
+        System.out.println(livro1);
       /*  int a = 5;
         int b= 3;
         System.out.println("Hello World " + (a+b));*/
 
+    }
 
+}
+
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
     }
 }
